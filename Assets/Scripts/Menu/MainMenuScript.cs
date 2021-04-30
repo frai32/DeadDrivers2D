5502c391b224d71e934af105f2fcff11a4e30dfd
llobachev <llobachev@game-forest.com>
2021-04-30 17:40:16 +1000

    add main menu

---
+++ Assets/Scripts/Menu/MainMenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void NextLevel(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
