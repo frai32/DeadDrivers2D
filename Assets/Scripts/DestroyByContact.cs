a4ca1c337fc29b5209223e287582d47b2930b77f
llobachev <llobachev@game-forest.com>
2021-09-09 17:25:25 +1000

    change tags

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
-    GameController localControl;
-    private AudioSource m_ExplosionAudio;
-
-    Animator anim;
     public int scoreValue;
 
+    private GameController localControl;
+    private AudioSource m_ExplosionAudio;
+
+    private Animator anim;
+
+    private Tags t;
   
     
     private void Awake()
     {
         anim = GetComponent<Animator>();
-
+        t = new Tags();
         m_ExplosionAudio = GetComponent<AudioSource>();
     }
 
 
     private void Start()
     {
-        GameObject gameControll = GameObject.FindGameObjectWithTag("GameController");
+        //GameObject gameControll = GameObject.FindGameObjectWithTag("GameController");
+        GameObject gameControll = GameObject.FindGameObjectWithTag(t.getTagList[(int)ETags.GAME_CONTROLLER_TAG]);
         if(gameControll != null)
         {
             localControl = gameControll.GetComponent<GameController>();
@@ -33,13 +36,15 @@ public class DestroyByContact : MonoBehaviour
             Debug.LogError("GameController is missed");
         }
     }
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Baundary"))
+        //if (collision.CompareTag("Baundary"))
+        if (collision.CompareTag(t.getTagList[(int)ETags.BAUNDARY_TAG]))
             return;
 
-        if(collision.CompareTag("Player"))
+        //if(collision.CompareTag("Player"))
+        if (collision.CompareTag(t.getTagList[(int)ETags.PLAYER_TAG]))
         {
             collision.GetComponent<health>().TakeDamage();
             OnDeath();
@@ -47,7 +52,8 @@ public class DestroyByContact : MonoBehaviour
           
         }
 
-        if (collision.CompareTag("Bolt"))
+       // if (collision.CompareTag("Bolt"))
+        if (collision.CompareTag(t.getTagList[(int)ETags.BOLT_TAG]))
         {
             Destroy(collision.gameObject);
             OnDeath();
